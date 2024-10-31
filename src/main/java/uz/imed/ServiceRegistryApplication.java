ccaf5b38174c327cf444a19b75e93d21499fc944
SuperHuma <theferganes@gmail.com>
2024-10-31 15:01:17 +0500

    creating

--- src/main/java/uz/imed/ServiceRegistryApplication.java
+++ src/main/java/uz/imed/ServiceRegistryApplication.java
@@ -6,11 +6,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServiceRegistryApplication {
+public class ServiceRegistryApplication
+{
 
-	public static void main(String[] args)
-	{
-		SpringApplication.run(ServiceRegistryApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
 
 }
